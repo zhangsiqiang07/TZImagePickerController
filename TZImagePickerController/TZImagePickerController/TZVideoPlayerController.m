2a30e5218ff7f75b9c6b7c8e66c67700b0f1bb91
啊强小衰flynn <1328254616@qq.com>
2026-03-02 20:01:52 +0800

    Configure audio session for video playback
    Set AVAudioSession category to playback and activate it before playing video.

--- TZImagePickerController/TZImagePickerController/TZVideoPlayerController.m
+++ TZImagePickerController/TZImagePickerController/TZVideoPlayerController.m
@@ -203,6 +203,9 @@
     CMTime currentTime = _player.currentItem.currentTime;
     CMTime durationTime = _player.currentItem.duration;
     if (_player.rate == 0.0f) {
+        NSError *error = nil;
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:&error];
+        [[AVAudioSession sharedInstance] setActive:YES error:&error];
         [[NSNotificationCenter defaultCenter] postNotificationName:@"TZ_VIDEO_PLAY_NOTIFICATION" object:_player];
         if (currentTime.value == durationTime.value) [_player.currentItem seekToTime:CMTimeMake(0, 1)];
         [_player play];
